d40123af425819662a4cdeed1a910b7c287ea8b8
Stephan <stephan@citrii.com>
2025-06-30 21:00:02 +0200

    feat: implement user authentication with JWT and create accounts controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,30 +3,27 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class UsersController(DataContext dataContext) : BaseApiController
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController(DataContext dataContext) : ControllerBase
-    {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-        { 
-            var users = await dataContext.Users.ToListAsync();
-            return users;
-        }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    { 
+        var users = await dataContext.Users.ToListAsync();
+        return users;
+    }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        { 
-            var user = await dataContext.Users.FindAsync(id);
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    { 
+        var user = await dataContext.Users.FindAsync(id);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
-            
-            return user;
+        if (user == null)
+        {
+            return NotFound();
         }
+        
+        return user;
     }
 }
