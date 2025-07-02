7d2b6fd71804fae16b2cc0ad98cb634f3b4bec44
Stephan <stephan@citrii.com>
2025-07-02 17:48:15 +0200

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountsService } from '../_services/accounts.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [FormsModule, BsDropdownModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  public readonly accountService = inject(AccountsService);
+  model: any = {};
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.error(error);
+      }
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountsService } from '../_services/accounts.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private readonly accountService = inject(AccountsService);
+  cancelRegister = output<boolean>();
+  model: any = {}
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => {
+        console.error(error);
+      }
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
